315fa577f519948e5bf3ec1300269877f0da7018
Ankur Seth <iankurseth@gmail.com>
2024-07-21 14:42:56 +0530

    Now composing candle_string in steps

--- pywax/wax_candle.py
+++ pywax/wax_candle.py
@@ -25,6 +25,10 @@ class WaxCandle:
 	def __str__(self):
 		datetime_string = self.datetime_string()
 		date, time = datetime_string.split(' ')
-		return f"| {self.idx+1:4} | {date} | {time} | {self.open:8.2f} | {self.high:8.2f} | {self.low:8.2f} | {self.close:8.2f} | {self.volume/1000_000:5.2f} M |"
+		candle_string = f"| {self.idx+1:4} | {date} | {time} |"
+		candle_string += f" {self.open:8.2f} | {self.high:8.2f} | {self.low:8.2f} | {self.close:8.2f} |"
+		candle_string += f" {self.volume/1000_000:5.2f} M |"
+		candle_string += f" {self.open_interest/1000_000:5.2f} M |"
+		return candle_string
 
 
