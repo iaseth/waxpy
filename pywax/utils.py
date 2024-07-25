6970f3ca3d4f1646cd3b3a7d6656fda8920cd97d
Ankur Seth <iankurseth@gmail.com>
2024-07-25 15:29:07 +0530

    Implemented get_encoder and get_decoder

--- pywax/utils.py
+++ pywax/utils.py
@@ -14,6 +14,20 @@ def intb(integer_bytes):
 	return integer
 
 
+def zeroes(n: int):
+	match(n):
+		case 0: return []
+		case 1: return [0]
+		case 2: return [0, 0]
+		case 3: return [0, 0, 0]
+		case 4: return [0, 0, 0, 0]
+		case 5: return [0, 0, 0, 0, 0]
+		case 6: return [0, 0, 0, 0, 0, 0]
+		case 7: return [0, 0, 0, 0, 0, 0, 0]
+		case 8: return [0, 0, 0, 0, 0, 0, 0, 0]
+		case _: return []
+
+
 def get_header_bytes(header_lines_count, column_count, row_length, row_count):
 	parts = [
 		bint(0, 2),
