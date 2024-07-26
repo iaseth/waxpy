9229a8c4f9b3d4f7f3e51c4a95c3dba8c2a382d7
Ankur Seth <iankurseth@gmail.com>
2024-07-26 13:02:08 +0530

    Now processing candles from data object

--- pywax/json_file.py
+++ pywax/json_file.py
@@ -20,6 +20,17 @@ def parts_from_candle_dict(candle):
 	]
 
 
+def parts_from_data_dict(data, idx):
+	return [
+		data['t'][idx],
+		data['o'][idx],
+		data['h'][idx],
+		data['l'][idx],
+		data['c'][idx],
+		data['v'][idx]
+	]
+
+
 def json_to_candles(jo):
 	candles = []
 	if 'candles' in jo and type(jo['candles']) is list and len(jo['candles']) > 0:
@@ -29,8 +40,10 @@ def json_to_candles(jo):
 			candles = [WaxCandle(parts) for parts in data]
 		elif type(first_data_point) is dict:
 			candles = [WaxCandle(parts_from_candle_dict(cj)) for cj in data]
-	elif 'data' in jo:
-		pass
+	elif 'data' in jo and type(jo['data']) is dict:
+		data = jo['data']
+		if 't' in data and 'o' in data and 'h' in data and 'l' in data and 'c' in data and 'v' in data:
+			candles = [WaxCandle(parts_from_data_dict(data, idx)) for idx, _ in enumerate(data['t'])]
 
 	return candles
 
